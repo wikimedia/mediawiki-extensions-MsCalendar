e0aa50c3ff478004a487151b4b892f7dbb10f6aa
Mark A. Hershberger <mah@nichework.com>
2017-09-27 14:33:37 -0400

    Fix MySQLism
    Reported here:
    https://www.mediawiki.org/w/index.php?title=Topic:Tpg1rnsqsaa0wsp0
    
    Change-Id: I422a640a59e5e23bb48e105499e328671dd48abd

--- MsCalendar.sql
+++ MsCalendar.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS /*_*/mscal_names (
-	ID int UNSIGNED NOT NULL auto_increment,
+	ID int NOT NULL auto_increment,
 	Cal_Name varchar(255) NOT NULL,
 	PRIMARY KEY (ID)
 );
 
 CREATE TABLE IF NOT EXISTS /*_*/mscal_list (
-	ID int UNSIGNED NOT NULL auto_increment,
+	ID int NOT NULL auto_increment,
 	Date DATE NOT NULL,
-	Text_ID int UNSIGNED NOT NULL,
-	Day_of_Set int UNSIGNED NOT NULL DEFAULT 1,
-	Cal_ID int UNSIGNED NOT NULL,
+	Text_ID int NOT NULL,
+	Day_of_Set int NOT NULL DEFAULT 1,
+	Cal_ID int NOT NULL,
 	PRIMARY KEY (ID)
 );
 
 CREATE TABLE IF NOT EXISTS /*_*/mscal_content (
-	ID int UNSIGNED NOT NULL auto_increment,
+	ID int NOT NULL auto_increment,
 	Text varchar(255) NOT NULL,
 	Start_Date DATE NOT NULL,
-	Duration int UNSIGNED NOT NULL,
-	Yearly int UNSIGNED NOT NULL,
+	Duration int NOT NULL,
+	Yearly int NOT NULL,
 	PRIMARY KEY (ID)
 );
